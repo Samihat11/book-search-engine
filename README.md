ca6bcc5cb825b7232b2d3b890823f3e32e590dd4
Samihat11 <sameerah11@hotmail.com>
2022-01-09 22:06:06 -0500

    refactored.

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# book-search-engine
+# Book-Search-Engine ![badge](https://img.shields.io/badge/MIT-license-blue)
+
+## Description
+
+A book-search app using Graphql and Apollo.
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+
+Clone this repo.
+Run `npm install`.
+Run `npm develop`.
+
+## License
+
+![badge](https://img.shields.io/badge/MIT-license-blue)
+
+This application is covered by the MIT License.
+
+## Questions
+
+If you have any questions, reach out to me via:
+
+GitHub: [Samihat11](https://github.com/Samihat11)
+
+Email: [sabdalla.dev@gmail.com](mailto:sabdalla.dev@gmail.com)
---
+++ client/build/precache-manifest.295579dc2372a3d28511ece51e1b351a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30471d9ce5ea633e2940e6af3a781cb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6d9dfe41ff652a04c59",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "6190a0e4f4cd5db91dca",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "d6d9dfe41ff652a04c59",
+    "url": "/static/js/2.8b5a6783.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.8b5a6783.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6190a0e4f4cd5db91dca",
+    "url": "/static/js/main.29f8ae00.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
